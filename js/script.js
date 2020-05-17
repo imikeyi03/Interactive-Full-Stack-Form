da7bf50f7320155df974549cbcee13151dca3538
Mikey Mann <mikey.mannx3@gmail.com>
2020-05-17 10:45:05 -0700

    working on t-shirt info section

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // GLOBAL VARIABLES
 let jobRoleSelection = document.getElementById('title'); 
 let otherJobOption = document.getElementById('other-title');
+let colorThemeSelection = document.getElementById('color');
 
 
 
@@ -44,6 +45,43 @@ jobRoleSelection.addEventListener("change", (e) => {
 
 
 
+// T-Shirt Info section
+
+
+
+// Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field
+// reads “Please select a T-shirt theme”.
+
+function watchColor(color) {
+    for(let i = 0; i < color.length; i++) {
+        color[i].hidden = true;
+    }
+    color += document.createElement('option');
+    color += document.createTextNode("Please select a T-shirt theme")
+    console.log(color);
+}
+
+
+
+// For the T-Shirt "Color" menu, after a user selects a theme, only display the color options that match the design
+// selected in the "Design" menu.
+
+
+
+// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue,"
+// "Dark Slate Grey," and"Gold."
+
+
+
+// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato,"
+// "Steel Blue," and "Dim Grey."
+
+
+
+// When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated.
+
+
+
 
 
 
@@ -58,3 +96,4 @@ jobRoleSelection.addEventListener("change", (e) => {
 
 
 hideAltJobInput();
+watchColor(colorThemeSelection);
