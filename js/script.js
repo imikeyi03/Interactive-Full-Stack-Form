b5dd3c4448424755bf339977889631e33be62259
Mikey Mann <mikey.mannx3@gmail.com>
2020-05-17 09:38:15 -0700

    Added job role selection event listener and cleaned up hidealt Function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+'use strict'
+
+// GLOBAL VARIABLES
+let jobRoleSelection = document.getElementById('title'); 
+let otherJobOption = document.getElementById('other-title');
+
+
+
+
 //Function to focus on the first text field on page load
 
 //wait until page load event has fired
@@ -11,16 +20,26 @@ window.onload = function initalFocus() {
 
 
 //Function to hide alternative form input field if JS is disabled
-
-function altJobInput() {
-    let altInput = document.getElementById('other-title');
-    altInput.classList.add('is-hidden');
+function hideAltJobInput() {
+    otherJobOption.classList.add('is-hidden');
 };
 
 
 
 
 
+// Reveal other textbox if other job role is selected
+
+
+jobRoleSelection.addEventListener("change", (e) => { 
+    for(let i = 0; i < jobRoleSelection.length; i++) {
+        if(e.target.value == 'other') {
+            otherJobOption.classList.remove('is-hidden');
+        } else {
+            otherJobOption.classList.add('is-hidden');
+        }
+    }
+});
 
 
 
@@ -38,5 +57,4 @@ function altJobInput() {
 
 
 
-
-altJobInput();
+hideAltJobInput();
