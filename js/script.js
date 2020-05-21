e6ff3b7f3c0594cb82b02fe21b52a4cd2125bd0d
Mikey Mann <mikey.mannx3@gmail.com>
2020-05-21 16:08:26 -0700

    finished t-shirt info selection changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let jobRoleSelection = document.getElementById('title');
 let otherJobOption = document.getElementById('other-title');
 let colorThemeSelection = document.getElementById('color');
 let colorOptions = colorThemeSelection.options;
+let designSelection = document.getElementById('design');
 
 //Function to focus on the first text field on page load
 
@@ -25,12 +26,8 @@ function hideAltJobInput() {
 
 
 
-
-
 // Reveal other textbox if other job role is selected
-
-
-jobRoleSelection.addEventListener("change", (e) => { 
+jobRoleSelection.addEventListener('change', (e) => { 
     for(let i = 0; i < jobRoleSelection.length; i++) {
         if(e.target.value == 'other') {
             otherJobOption.classList.remove('is-hidden');
@@ -45,44 +42,60 @@ jobRoleSelection.addEventListener("change", (e) => {
 
 // T-Shirt Info section
 
-
-
 // Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field
 // reads “Please select a T-shirt theme”.
-let optionNode = document.createElement('option');
-let defaultColor = document.createTextNode('Please select a T-shirt theme');
-optionNode.appendChild(defaultColor);
-document.getElementById('color').prepend(optionNode)
-
+const optionNode = document.createElement('option');
+const defaultColorMessage = document.createTextNode('Please select a T-shirt theme');
+optionNode.appendChild(defaultColorMessage);
+document.getElementById('color').prepend(optionNode);
 
 for(let i = 0; i < colorOptions.length; i++ ) {
-    
     if(colorOptions[i].value === "Please select a T-shirt theme" ) {
+        colorOptions[i].selected = true;
         colorOptions[i].hidden = false;
     } else {
         colorOptions[i].hidden = true;
     }
 }
  
-
-
-
 // For the T-Shirt "Color" menu, after a user selects a theme, only display the color options that match the design
 // selected in the "Design" menu.
 
+const jsPun = designSelection.options[1]; 
+const heartJS = designSelection.options[2]; 
 
+designSelection.options[0].selected = true;
 
-// If the user selects "Theme - JS Puns" then the color menu should only display "Cornflower Blue,"
-// "Dark Slate Grey," and"Gold."
+designSelection.addEventListener('change', (event) => {
+    
+    //hide default params
+    colorOptions[0].hidden = true;
+    designSelection.options[0].hidden = true;
 
+ //if the chosen value is js puns, only show top three indexes
+        if(jsPun.selected == true) {
+            
+            for (let i = 0; i < colorOptions.length; i++) {
+                if (i > 0 && i <=3) {
+                    colorOptions[i].hidden = '';
+                } else {
+                    colorOptions[i].hidden = true;
+                }
+            }
+        }
 
-
-// If the user selects "Theme - I ♥ JS" then the color menu should only display "Tomato,"
-// "Steel Blue," and "Dim Grey."
-
-
-
-// When a new theme is selected from the "Design" menu, both the "Color" field and drop down menu is updated.
+    //if the chosen value is I <3 JS, only show bottom three indexes
+    if(heartJS.selected == true) {
+            
+        for (let i = 0; i < colorOptions.length; i++) {
+            if (i > 3 && i <=6) {
+                colorOptions[i].hidden = '';
+            } else {
+                colorOptions[i].hidden = true;
+            }
+        }    
+    }
+});
 
 
 
